65417a11e4ccfa9feb56fa97c3574ece1050ca55
John Hills <146108747+JohnHillsCodeProjects@users.noreply.github.com>
2023-11-05 15:44:45 +0000

    Delete Chapter 7 managing projects directory

--- Chapter 7 managing projects/growing_with_cargo/garden.rs
+++
@@ -1,4 +0,0 @@
-//This is the attached code file for the garden module, where you can define submodules.
-//If you wnat submodules to have code defined in a different file, then you'll have to create a 
-
-pub mod flower; //This is a submodule of the garden module. 
